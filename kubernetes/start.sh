7d96658c7c8106a05b0ce970ec60430007fdd32b
Edi Sinovčić <edi.sinovcic@gmail.com>
2019-05-29 16:48:48 +0200

    Updating basic scripts

--- kubernetes/start.sh
+++ kubernetes/start.sh
@@ -2,10 +2,6 @@
 
 ./kuberneteth
 
-kubectl delete -f deployment.yaml && \\
-sleep 20 && \\
-kubectl delete -f claims.yaml && \\
-sleep 20 && \\
 kubectl apply -f claims.yaml && \\
 sleep 20 && \\
 kubectl apply -f deployment.yaml
